cee6b6ae00a38dc5ab9f4a08b9d014828fb079f0
GURPREET SINGH <singhsarpreet234@gmail.com>
2024-05-04 16:55:08 +0530

    new content added

--- Basics/if_else.py
+++ Basics/if_else.py
@@ -1,17 +1,27 @@
-        
-# if else statement
+# This is an example of an if-else statement in Python
 x = 10
+# The variable 'x' is assigned the value of 10
+
+# The if statement checks if x is greater than 10
 if x > 10:
+    # If x is greater than 10, print the following message
     print("x is greater than 10")
 else:
+    # If x is less than or equal to 10, print the following message
     print("x is less than or equal to 10")
 
-# if elif else statement
+# This is an example of an if-elif-else statement in Python
 x = 10
+# The variable 'x' is assigned the value of 10
+
+# The if statement checks if x is greater than 10
 if x > 10:
+    # If x is greater than 10, print the following message
     print("x is greater than 10")
+# The elif statement checks if x is equal to 10
 elif x == 10:
+    # If x is equal to 10, print the following message
     print("x is equal to 10")
 else:
+    # If x is less than 10, print the following message
     print("x is less than 10")
-
---
+++ Basics/tuet_1.py
@@ -0,0 +1,10 @@
+x = 10
+print(type(x))
+
+x = "str"
+print(type(x))
+
+
+n = '3'
+n1 = int(n,2)
+print(n1)
